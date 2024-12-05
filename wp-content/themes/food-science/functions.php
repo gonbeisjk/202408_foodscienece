9544a4649c80d7d390fa77af2309dd7fc82ebfa6
taro shinjyuku <tarosjk@gmail.com>
2024-12-05 10:40:14 +0900

    アイキャッチ画像を有効化、TOPページで表示

--- wp-content/themes/food-science/functions.php
+++ wp-content/themes/food-science/functions.php
@@ -5,6 +5,14 @@
  */
 add_theme_support('title-tag');
 
+/**
+ * アイキャッチ画像を使用可能にする
+ */
+add_theme_support('post-thumbnails');
+
+/**
+ * タイトルの区切り文字を変更
+ */
 add_filter('document_title_separator', 'my_document_title_separator');
 function my_document_title_separator()
 {
