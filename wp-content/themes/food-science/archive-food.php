2f7802ff9ea0d03623580fbce3af6a2a8ed7553a
taro shinjyuku <tarosjk@gmail.com>
2024-12-11 11:38:32 +0900

    フード一覧ページをメニュー毎に出力するように変更

--- wp-content/themes/food-science/archive-food.php
+++ wp-content/themes/food-science/archive-food.php
@@ -7,26 +7,51 @@
         <h2 class="heading heading-primary"><span>フード紹介</span>FOOD</h2>
       </div>
 
-      <section class="section_body">
-        <h3 class="heading heading-secondary">お食事<span>MEAL</span></h3>
-        <ul class="foodList">
-          <?php if (have_posts()): ?>
-            <?php while (have_posts()): the_post(); ?>
-              <li class="foodList_item">
-                <?php get_template_part('template-parts/loop', 'food'); ?>
-              </li>
-          <?php endwhile;
-          endif; ?>
+      <?php
+      $menu_terms = get_terms([
+        'taxonomy' => 'menu',
+      ]);
+      ?>
+      <?php if (!empty($menu_terms)): ?>
+        <?php foreach ($menu_terms as $menu): ?>
+          <section class="section_body">
+            <h3 class="heading heading-secondary">
+              <a href="<?= get_term_link($menu); ?>"><?= $menu->name; ?></a><span><?= strtoupper($menu->slug); ?></span>
+            </h3>
+            <ul class="foodList">
+              <?php
+              $args = [
+                'post_type' => 'food', //「food」投稿タイプ限定
+                'posts_per_page' => -1, //全件取得
+                // ↓タクソノミーに関する条件指定
+                'tax_query' => [
+                  'relation' => 'AND',
+                  // 条件1
+                  [
+                    'taxonomy' => 'menu',
+                    'field' => 'slug',
+                    'terms' => $menu->slug,
+                  ],
+                  // 条件2, 3, ...があった場合
+                  // [], [], ...
+                ],
+              ];
+              $the_query = new WP_Query($args);
+              ?>
+              <?php if ($the_query->have_posts()): ?>
+                <?php while ($the_query->have_posts()): $the_query->the_post(); ?>
+                  <li class="foodList_item">
+                    <?php get_template_part('template-parts/loop', 'food'); ?>
+                  </li>
+              <?php endwhile;
+                wp_reset_postdata();
+              endif; ?>
 
-        </ul>
-      </section>
+            </ul>
+          </section>
+        <?php endforeach; ?>
+      <?php endif; ?>
 
-      <section class="section_body">
-        <h3 class="heading heading-secondary">ドリンク<span>DRINK</span></h3>
-        <ul class="foodList">
-
-        </ul>
-      </section>
     </div>
   </section>
 </main>
